25a324b38814f201c1f3bcf85e596f540d7a800c
Surendra Shiwakoti <63626177+shiwakotisurendra@users.noreply.github.com>
2023-07-23 11:41:13 +0200

    Update as1.py

--- as1.py
+++ as1.py
@@ -147,40 +147,40 @@ with col1:
             attr="ESRI Imagery",
             name="ESRI Imagery",
         ),
-         CyclOSM: L.tileLayer(
+         "CyclOSM": folium.TileLayer(
           "https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
           {
-            maxZoom: 20,
+            "maxZoom": 20,,
             attribution:
               '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
           }
         ),
-        googleStreets: L.tileLayer(
+        "googleStreets": folium.TileLayer(
           "http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
           {
-            maxZoom: 20,
+            "maxZoom": 20,
             subdomains: ["mt0", "mt1", "mt2", "mt3"],
           }
         ),
-        googleSat: L.tileLayer(
+        "googleSat": folium.TileLayer(
           "http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
           {
-            maxZoom: 20,
+            "maxZoom": 20,
             subdomains: ["mt0", "mt1", "mt2", "mt3"],
           }
         ),
-        googleHybrid: L.tileLayer(
+        "googleHybrid": folium.TileLayer(
           "http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}",
           {
-            maxZoom: 20,
+            "maxZoom": 20,
             subdomains: ["mt0", "mt1", "mt2", "mt3"],
           }
         ),
-        googleTerrain: L.tileLayer(
+        "googleTerrain": folium.TileLayerr(
           "http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}",
           {
-            maxZoom: 20,
-            subdomains: ["mt0", "mt1", "mt2", "mt3"],
+            "maxZoom": 20,
+            "subdomains": ["mt0", "mt1", "mt2", "mt3"],
           }
         ),
     }
